48738cd349ddc71b46f87ea254cd9c4c5ad21913
CMatvey <chekashovmatvey@gmail.com>
2021-09-29 22:52:28 +0300

    In menu add link to admin panel for site staff

--- CourseMC/urls.py
+++ CourseMC/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('coursemc_control/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('', include('social_django.urls')),
