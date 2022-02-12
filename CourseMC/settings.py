fdbf54b91d08e5055086bb625fb92bf4fdd3125c
Чекашов Матвей Павлович <chekashovmatvey@gmail.com>
2022-02-12 12:11:57 +0300

    Update settings.py

--- CourseMC/settings.py
+++ CourseMC/settings.py
@@ -28,7 +28,7 @@ LOGIN_REDIRECT_URL = '/'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['coursemc.space', 'www.coursemc.space', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 # Application definition
 
