fb077a661896a9acd640067f0b526c8f4b7b7338
Чекашов Матвей Павлович <chekashovmatvey@gmail.com>
2022-07-21 10:56:18 +0300

    Delete Course directory

--- Course/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from Course.views import *
-
-urlpatterns = [
-    path('', StudentRecordView.as_view(), name='home'),
-    path('schedule/', TimetableView.as_view(), name='schedule'),
-]
