9a4e160856726e3676563c4db801cacf661f3463
Чекашов Матвей Павлович <chekashovmatvey@gmail.com>
2022-07-23 19:16:50 +0300

    Added new admin panel (django-jazzmin)

--- CourseMC/settings.py
+++ CourseMC/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['coursemc.space', 'www.coursemc.space', 'localhost', '127.0.0.1
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
