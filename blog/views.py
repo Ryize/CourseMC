ec11ac34b95c414c6055873d5284d185568f046b
Чекашов Матвей Павлович <chekashovmatvey@gmail.com>
2022-11-05 20:50:25 +0300

    Добавлены фильтры на статьи

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import JsonResponse
-from django.shortcuts import redirect
 from django.views.generic import CreateView, DetailView, ListView
-
 from .forms import PostForm
 from .models import *
 
@@ -31,6 +29,12 @@ class PostListView(ListView):
             .prefetch_related("categories")
             .all()
         )
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["Category"] = Category.objects.all()
+
+        return context
 
 
 class MyPostListView(LoginRequiredMixin, ListView):
