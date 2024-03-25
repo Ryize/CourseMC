6d98f196facb9dd72bb085a017a1c87e20c4b28e
matvejcekasov <chekashovmatvey@gmail.com>
2024-03-25 09:03:13 +0300

    feat: Добавлена модель для сохранения статистики по прибыли и активным ученикам

--- statistic/models.py
+++ statistic/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+from Course.models import Student
+
+
+class Payment(models.Model):
+    """
+    Модель для хранения данных по прибыли и ученикам, которые её приносят.
+    """
+    profit = models.PositiveIntegerField(verbose_name='Прибыль')
+    students = models.ManyToManyField(Student, related_name='active_students',
+                                      on_delete=models.PROTECT)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата')
+
+    def __str__(self):
+        return f'Прибыль на {self.created_at}: {self.profit}'
+
+    class Meta:
+        verbose_name = 'Прибыль'
+        verbose_name_plural = 'Прибыль'
